dcf70d2373168d091ad9f65ae540993085f79e68
EversonDoNascimento <everson25nascimento@gmail.com>
2025-07-18 10:58:35 -0300

    feat: Iniciando projeto e instalando Typescript

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from "fastify";
+
+const server = fastify({
+  logger: true,
+});
+
+server.listen({ port: 3000 }, (err, address) => {
+  if (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+  server.log.info(`Server listening at ${address}`);
+});
+
+export default server;
