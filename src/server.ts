0448b31e704079a936899f1c876d3946608735fc
EversonDoNascimento <everson25nascimento@gmail.com>
2025-07-20 11:57:49 -0300

    feat: Importando arquivo de rotas e utilizando a variável de ambiente que possui a porta do servidor

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,23 @@
 import fastify from "fastify";
+import dotenv from "dotenv";
+import paymentRoutes from "./interfaces/http/routes/paymentRoutes";
+dotenv.config();
 
 const server = fastify({
   logger: true,
 });
 
-server.listen({ port: 3000 }, (err, address) => {
-  if (err) {
-    server.log.error(err);
-    process.exit(1);
+server.register(paymentRoutes);
+
+server.listen(
+  { port: parseInt(process.env.SERVER_PORT || "3000") },
+  (err, address) => {
+    if (err) {
+      server.log.error(err);
+      process.exit(1);
+    }
+    server.log.info(`Server listening at ${address}`);
   }
-  server.log.info(`Server listening at ${address}`);
-});
+);
 
 export default server;
