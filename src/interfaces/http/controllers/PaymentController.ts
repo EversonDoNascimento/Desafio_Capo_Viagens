09a2eb03b7bb4d468363a40c19da37808c8ae9c1
EversonDoNascimento <everson25nascimento@gmail.com>
2025-07-21 10:32:30 -0300

    feat: Implementar métodos para criar um pagamento e buscar um pagamento por ID

--- src/interfaces/http/controllers/PaymentController.ts
+++ src/interfaces/http/controllers/PaymentController.ts
@@ -1,6 +1,12 @@
 import { FastifyRequest, FastifyReply } from "fastify";
-import { SchemaCreatePayment } from "../../../shared/schemasValidations/PaymentSchemas";
+import {
+  SchemaCreatePayment,
+  SchemaFindPaymentById,
+} from "../../../shared/schemasValidations/PaymentSchemas";
 import z from "zod";
+import MySQLPaymentRepository from "../../../infrastructure/db/mysql/MySQLPaymentRepository";
+import CreatePayment from "../../../application/use-cases/CreatePayment";
+import { FindPaymentById } from "../../../application/use-cases/FindPaymentById";
 
 export default class PaymentController {
   async healthCheck() {
@@ -15,9 +21,37 @@ export default class PaymentController {
       });
       return;
     }
-    const { method, amount, card, buyer } = request.body as z.infer<
-      typeof SchemaCreatePayment
-    >;
-    return { message: "Payment processed successfully" };
+    const data = request.body as z.infer<typeof SchemaCreatePayment>;
+
+    const mysqlPayment = new MySQLPaymentRepository();
+    const createPaymentUseCase = new CreatePayment(mysqlPayment);
+
+    const created = await createPaymentUseCase.execute(data);
+    if (!created.success) {
+      return reply.status(400).send({ error: "Falha ao criar o pagamento" });
+    }
+    return reply.status(201).send({
+      message: "Pagamento criado com sucesso!",
+      id: created.id,
+    });
+  }
+  async findPaymentById(request: FastifyRequest, reply: FastifyReply) {
+    const validation = SchemaFindPaymentById.safeParse(request.params);
+    if (!validation.success) {
+      reply
+        .status(400)
+        .send({ error: "Invalid ID format", issues: validation.error.issues });
+      return;
+    }
+
+    const data = request.params as z.infer<typeof SchemaFindPaymentById>;
+    const mysqlPayment = new MySQLPaymentRepository();
+    const findPaymentByIdUseCase = new FindPaymentById(mysqlPayment);
+    const result = await findPaymentByIdUseCase.execute(data.id);
+    if (!result) {
+      return reply.status(404).send({ error: "Pagamento não encontrado" });
+    }
+
+    return reply.status(200).send(result);
   }
 }
