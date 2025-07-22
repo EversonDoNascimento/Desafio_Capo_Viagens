e1b51135353066e16572de9e14730910e8859e58
EversonDoNascimento <everson25nascimento@gmail.com>
2025-07-22 13:19:09 -0300

    feat: Implementando regra de negócio para encriptar dados do cartão de crédito antes de salvar no banco de dados

--- src/application/use-cases/CreatePayment.ts
+++ src/application/use-cases/CreatePayment.ts
@@ -2,15 +2,25 @@ import z from "zod";
 import Payment from "../../domain/entities/Payment";
 import { SchemaCreatePayment } from "../../shared/schemasValidations/PaymentSchemas";
 import PaymentRepository from "../../domain/repositories/PaymentRepository";
+import { encrypt } from "../../shared/utils/crypt";
 
 export default class CreatePayment {
   constructor(private paymentRepository: PaymentRepository) {
     this.paymentRepository = paymentRepository;
   }
 
-  async execute(paymentData: z.infer<typeof SchemaCreatePayment>) {
+  async execute(
+    paymentData: z.infer<typeof SchemaCreatePayment>
+  ): Promise<{ id?: number; success: boolean }> {
     const payment = new Payment(paymentData);
-    await this.paymentRepository.create(payment);
-    return payment;
+    if (payment.getMethod() == "credit_card" && paymentData.card) {
+      const infoCardCrypted = {
+        encryptedData: encrypt(paymentData.card.encryptedData),
+      };
+      payment.setCard(infoCardCrypted);
+    }
+    const created = await this.paymentRepository.create(payment);
+
+    return created;
   }
 }
