b6477dbbe8d6fb21d85af67eaa4c27f357484f7e
EversonDoNascimento <everson25nascimento@gmail.com>
2025-07-22 13:29:23 -0300

    feat: Importando e utilizando o método createRefundTable

--- src/scripts/migrate.ts
+++ src/scripts/migrate.ts
@@ -1,8 +1,10 @@
 import { createPaymentTable } from "../infrastructure/db/migrations/createPaymentTable";
+import { createRefundTable } from "../infrastructure/db/migrations/createRefundTable";
 
 async function migrate() {
   try {
     await createPaymentTable();
+    await createRefundTable();
     console.log("Migration completed successfully.");
     process.exit(0);
   } catch (error) {
