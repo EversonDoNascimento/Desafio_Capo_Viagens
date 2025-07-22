f7ed05d5b50fb2713890bf1aeb586fb1bcaf943e
EversonDoNascimento <everson25nascimento@gmail.com>
2025-07-22 13:21:45 -0300

    feat: Adicionando método modifyStatusPayment para modificar o status do pagamento

--- src/domain/repositories/PaymentRepository.ts
+++ src/domain/repositories/PaymentRepository.ts
@@ -3,4 +3,8 @@ import Payment from "../entities/Payment";
 export default abstract class PaymentRepository {
   abstract create(payment: Payment): Promise<{ id?: number; success: boolean }>;
   abstract findById(id: number): Promise<Payment | null>;
+  abstract modifyStatusPayment(
+    id: number,
+    status: "pending" | "completed" | "failed"
+  ): Promise<Payment | null>;
 }
