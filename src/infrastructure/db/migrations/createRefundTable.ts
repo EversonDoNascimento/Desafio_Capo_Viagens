7f0c37f882abe24030a66c91b6fcdb2f3a6cf0d2
EversonDoNascimento <everson25nascimento@gmail.com>
2025-07-22 13:06:15 -0300

    feat: Criado Script para criar tabela refunds no banco de dados

---
+++ src/infrastructure/db/migrations/createRefundTable.ts
@@ -0,0 +1,18 @@
+import pool from "../connection";
+
+export async function createRefundTable() {
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS refunds (
+            id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+            payment_id INT NOT NULL,
+            FOREIGN KEY (payment_id) REFERENCES payments(id),
+            amount DECIMAL(10, 2) NOT NULL,
+            type ENUM('partial', 'full') NOT NULL,
+            status ENUM('pending', 'completed', 'failed') DEFAULT 'pending' NOT NULL,
+            refund_date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        );
+    `);
+  console.log("Refund table created successfully.");
+}
