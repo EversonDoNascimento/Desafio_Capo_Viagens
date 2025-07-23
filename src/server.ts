ff93829748b9b235a4ebcf7df1f5c578fcae7fdf
EversonDoNascimento <everson25nascimento@gmail.com>
2025-07-23 10:45:32 -0300

    feat: Importando e configurando o swagger

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,29 @@ import fastify from "fastify";
 import dotenv from "dotenv";
 import paymentRoutes from "./interfaces/http/routes/paymentRoutes";
 import refundRoutes from "./interfaces/http/routes/refundRoutes";
+import swagger from "@fastify/swagger";
+import swaggerUi from "@fastify/swagger-ui";
+import { swaggerConfig } from "./interfaces/http/swagger";
+
 dotenv.config();
 
 const server = fastify({
   logger: true,
 });
 
+server.register(swagger, swaggerConfig);
+server.register(swaggerUi, {
+  routePrefix: "/docs",
+  uiConfig: {
+    docExpansion: "full",
+    deepLinking: false,
+  },
+  staticCSP: true,
+  transformStaticCSP: (header) => header.replace("unsafe-inline", ""),
+});
 server.register(paymentRoutes);
 server.register(refundRoutes);
+
 server.listen(
   { port: parseInt(process.env.SERVER_PORT || "3000") },
   (err, address) => {
