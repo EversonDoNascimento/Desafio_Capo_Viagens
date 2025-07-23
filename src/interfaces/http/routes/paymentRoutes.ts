603aa4554292e6b63ab4ab020a918fd0142a6593
EversonDoNascimento <everson25nascimento@gmail.com>
2025-07-23 10:55:50 -0300

    feat: Importando schemas das rotas de pagamento do swagger

--- src/interfaces/http/routes/paymentRoutes.ts
+++ src/interfaces/http/routes/paymentRoutes.ts
@@ -1,17 +1,29 @@
 import { FastifyInstance } from "fastify";
 import PaymentController from "../controllers/PaymentController";
+import {
+  createPaymentSchema,
+  getPaymentByIDSchema,
+  healthCheckSchema,
+  modifyStatusPaymentSchema,
+} from "../schemas/paymentSchemas";
 
-export default async function paymentRoutes(server: FastifyInstance) {
+export default function paymentRoutes(server: FastifyInstance) {
   const paymentController = new PaymentController();
 
-  server.get("/health", () => paymentController.healthCheck());
-  server.post("/payments", (request, reply) =>
+  server.get("/health", { schema: healthCheckSchema }, (_, reply) =>
+    paymentController.healthCheck(reply)
+  );
+  server.post("/payments", { schema: createPaymentSchema }, (request, reply) =>
     paymentController.create(request, reply)
   );
-  server.get("/payments/:id", async (request, reply) =>
-    paymentController.findPaymentById(request, reply)
+  server.get(
+    "/payments/:id",
+    { schema: getPaymentByIDSchema },
+    (request, reply) => paymentController.findPaymentById(request, reply)
   );
-  server.patch("/payments/status/:id", (request, reply) =>
-    paymentController.modifyStatusPayment(request, reply)
+  server.patch(
+    "/payments/status/:id",
+    { schema: modifyStatusPaymentSchema },
+    (request, reply) => paymentController.modifyStatusPayment(request, reply)
   );
 }
