d6b9ac5283a8dda83f0daf2ac3789ee359607436
EversonDoNascimento <everson25nascimento@gmail.com>
2025-07-24 20:24:50 -0300

    feat: Criado Script sql para criação das tabelas

---
+++ tables.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS payments (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    amount DECIMAL(10, 2) NOT NULL,
+    method VARCHAR(255) NOT NULL,
+    status ENUM('pending', 'completed', 'failed') DEFAULT 'pending' NOT NULL,
+    card_number VARCHAR(255),
+    buyer_name VARCHAR(255) NOT NULL,
+    buyer_email VARCHAR(255) NOT NULL,
+    payment_date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    );
+
+CREATE TABLE IF NOT EXISTS refunds (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    payment_id INT NOT NULL,
+    FOREIGN KEY (payment_id) REFERENCES payments(id),
+    amount DECIMAL(10, 2) NOT NULL,
+    type ENUM('partial', 'full') NOT NULL,
+    status ENUM('pending', 'completed', 'failed') DEFAULT 'pending' NOT NULL,
+    refund_date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    );
