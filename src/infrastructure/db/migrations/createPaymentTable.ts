51978752553ad680cd43320e24b06d82cb99e240
EversonDoNascimento <everson25nascimento@gmail.com>
2025-08-11 15:09:48 -0300

    fix: Adicionando bloco finally para encerrar a conexão com o banco de dados

--- src/infrastructure/db/migrations/createPaymentTable.ts
+++ src/infrastructure/db/migrations/createPaymentTable.ts
@@ -2,19 +2,25 @@ import pool from "../connection";
 
 // Função para criar a tabela de pagamentos
 export async function createPaymentTable() {
-  await pool.query(`
-        CREATE TABLE IF NOT EXISTS payments (
-            id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-            amount DECIMAL(10, 2) NOT NULL,
-            method VARCHAR(255) NOT NULL,
-            status ENUM('pending', 'completed', 'failed') DEFAULT 'pending' NOT NULL,
-            card_number VARCHAR(255),
-            buyer_name VARCHAR(255) NOT NULL,
-            buyer_email VARCHAR(255) NOT NULL,
-            payment_date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-        );
-    `);
-  console.log("Payment table created successfully.");
+  try {
+    await pool.query(`
+          CREATE TABLE IF NOT EXISTS payments (
+              id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+              amount DECIMAL(10, 2) NOT NULL,
+              method VARCHAR(255) NOT NULL,
+              status ENUM('pending', 'completed', 'failed') DEFAULT 'pending' NOT NULL,
+              card_number VARCHAR(255),
+              buyer_name VARCHAR(255) NOT NULL,
+              buyer_email VARCHAR(255) NOT NULL,
+              payment_date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+              created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+              updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+          );
+      `);
+    console.log("Payment table created successfully.");
+  } catch (error) {
+    console.error("Error creating payment table:", error);
+  } finally {
+    await pool.end();
+  }
 }
